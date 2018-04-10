7d264c2228ad2c326a37c63ecca166214ea4f491
873668552 <873668552@qq.com>
2018-04-10 20:19:20 +0800

    vue

---
+++ vueProject/my-project/src/components/first.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      {{message}}
+  </div>
+</template>
+<script>
+export default {
+  name:'first',
+  data(){
+      return {
+          message:'hello vue'
+      }
+  }
+}
+</script>
+// scoped 只能被当组件使用
+<style scoped>
+
+</style>
+
---
+++ vueProject/my-project/src/components/reg.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>注册页面</h1>
+  </div>
+</template>
+<script>
+export default {
+    name:'reg'
+}
+</script>
+<style scoped>
+    h1{
+        background: green;
+    }
+</style>
+
+
